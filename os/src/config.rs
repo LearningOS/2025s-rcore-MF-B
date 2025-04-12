65c4caf81f8a19c47046275219bd83a3c998d50d
mf1bzz <guzemin@hotmail.com>
2025-04-12 17:11:42 +0800

    lab3

--- os/src/config.rs
+++ os/src/config.rs
@@ -21,3 +21,8 @@ pub const TRAP_CONTEXT_BASE: usize = TRAMPOLINE - PAGE_SIZE;
 pub const CLOCK_FREQ: usize = 12500000;
 /// the physical memory end
 pub const MEMORY_END: usize = 0x88000000;
+
+/// default priority of the new task
+pub const DEFAULT_TASK_PRIORITY: usize = 16;
+/// bigstride for task scheduling
+pub const BIGSTRIDE: usize = 1 << 16; 
--- os/src/mm/page_table.rs
+++ os/src/mm/page_table.rs
@@ -158,6 +158,14 @@ impl PageTable {
     pub fn token(&self) -> usize {
         8usize << 60 | self.root_ppn.0
     }
+    /// is mapped
+    pub fn is_mapped(&self, vpn: VirtPageNum) -> bool {
+        if let Some(pte)=self.find_pte(vpn) {
+            pte.is_valid()
+        } else {
+            false
+        }
+    }
 }
 
 /// Translate&Copy a ptr[u8] array with LENGTH len to a mutable u8 Vec through page table
